3b0583b84161bc66ea97b965f4060fd4f36515f9
Rain Liu <liuwenbin168@126.com>
2018-06-07 14:18:37 +0800

    Implement PM25View

--- app/src/main/java/com/hencoder/hencoderpracticelayout2/sample/PM25View.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout2/sample/PM25View.java
@@ -2,15 +2,21 @@ package com.hencoder.hencoderpracticelayout2.sample;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.View;
+
+
+import static android.view.View.MeasureSpec.EXACTLY;
+import static android.view.View.MeasureSpec.getMode;
 
 /**
  * Created by wliu on 05/06/2018.
@@ -18,33 +24,39 @@ import android.view.View;
 
 public class PM25View extends View {
 
+    private static int mcount = 0;
+
+    private static int STROKE_WIDTH = 30;
+    private static int SWEEP_COLOR_START = Color.BLUE;
+    private static int SWEEP_COLOR_END = Color.CYAN;
+
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mPaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    private float pollution = 117;
+    private int mWidth, mHeight, mPadding;
+
+    private float pollution, polutionValue = 300;
 
     {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setColor(Color.LTGRAY);
-        mPaint.setStrokeWidth(20);
+        mPaint.setStrokeWidth(STROKE_WIDTH);
 
         mPaint2.setStyle(Paint.Style.STROKE);
         mPaint2.setStrokeCap(Paint.Cap.ROUND);
-        mPaint2.setColor(Color.BLUE);
-        mPaint2.setStrokeWidth(20);
-
-        ObjectAnimator animator = ObjectAnimator.ofFloat(this, "pollution", 0, 117);
+        mPaint2.setStrokeWidth(STROKE_WIDTH);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(this, "pollution", 0, polutionValue);
         animator.setDuration(1000);
         animator.start();
     }
 
     public PM25View(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public PM25View(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public PM25View(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -57,16 +69,64 @@ public class PM25View extends View {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        ++mcount;
+        Log.w("xx", "mcount == " + mcount);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        switch (widthMode) {
+            case MeasureSpec.UNSPECIFIED:
+                Log.w("xx", "width mode == unspecified");
+                break;
+            case MeasureSpec.AT_MOST:
+                Log.w("xx", "width mode == at most");
+                break;
+            case EXACTLY:
+                Log.w("xx", "width mode == exactly");
+                calculateSizeExactly();
+                break;
+        }
+    }
+
+    /**
+     *
+     */
+    private void calculateSizeExactly() {
+        int w = getMeasuredWidth();
+        int h = getMeasuredHeight();
+        setMeasuredDimension(w, h);
+
+        initNumbers(w, h);
+    }
+
+    /**
+     *
+     * @param width
+     * @param height
+     */
+    private void initNumbers(int width, int height) {
+        mWidth = width;
+        mHeight = height;
+
+        mPadding = getPaddingTop();
+
+        // Sets the sweep gradient
+        mPaint2.setShader(new SweepGradient(mWidth / 2, mHeight / 2, SWEEP_COLOR_START, SWEEP_COLOR_END));
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // canvas.drawLine(100, 100, 500, 100, mPaint);
-        RectF rectF = new RectF(100, 100, 300, 300);
+        int left = STROKE_WIDTH + mPadding;
+        int top = STROKE_WIDTH + mPadding;
+        int right = mWidth - STROKE_WIDTH - mPadding;
+        int bottom = mHeight - STROKE_WIDTH - mPadding;
+        RectF rectF = new RectF(left, top, right, bottom);
         canvas.drawArc(rectF, 135, 270, false, mPaint);
 
 
         float endAngle = this.pollution / 500 * 270;
-        Log.e("x", "end angle == " + endAngle);
         canvas.drawArc(rectF, 135, endAngle, false, mPaint2);
     }
 }
